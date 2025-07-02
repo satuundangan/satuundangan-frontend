9925bd8559a3c9146faaa43b6a5cc13e2c421a52
fauzanlm <fauzan14072004@gmail.com>
2025-07-02 11:50:25 +0700

    Update mobile responsive

--- src/components/NavbarSection.vue
+++ src/components/NavbarSection.vue
@@ -1,13 +1,48 @@
 <template>
   <nav class="sticky top-0 z-50 bg-ivory shadow-sm  border-mocha px-6 py-4">
-    <div class="max-w-7xl mx-auto flex justify-between items-center">
+
+    <div class="max-w-7xl mx-auto flex md:justify-between justify-start items-center">
+      <button @click="sidebarOpen = true" class="md:hidden text-mocha text-2xl">
+        ☰
+      </button>
+      <!-- Sidebar (Mobile) -->
+      <transition name="fade">
+        <div v-if="sidebarOpen" class="fixed inset-0 z-40 bg-black/50 md:hidden" @click="sidebarOpen = false">
+          <aside class="fixed left-0 top-0 bottom-0 w-64 bg-white p-6 shadow-md z-50" @click.stop>
+            <div class="flex justify-between items-center mb-6">
+              <h2 class="text-lg font-bold text-mocha">Menu</h2>
+              <button @click="sidebarOpen = false" class="text-xl font-bold text-gray-400">×</button>
+            </div>
+
+            <ul class="space-y-4 text-dark">
+              <li><a href="#" class="block hover:text-mocha">Home</a></li>
+              <li><a href="#" class="block hover:text-mocha">Fitur</a></li>
+              <li><a href="#" class="block hover:text-mocha">Tema</a></li>
+              <li><a href="#" class="block hover:text-mocha">Harga</a></li>
+              <li><a href="#" class="block hover:text-mocha">Portofolio</a></li>
+              <li><a href="#" class="block hover:text-mocha">Blog</a></li>
+              <li v-if="userName">
+                <button @click="handleLogout" class="text-red-500 hover:underline">Logout</button>
+              </li>
+              <li v-else>
+                <button @click="() => { sidebarOpen = false; show.value = true }"
+                  class="hover:text-mocha">Login</button>
+                <button @click="() => { emit('create-invitation'); sidebarOpen = false }" class="btn-primary mt-2">
+                  Buat Undangan
+                </button>
+              </li>
+            </ul>
+          </aside>
+        </div>
+      </transition>
+
       <!-- Logo -->
       <div class="flex items-center gap-3">
         <div
-          class="bg-sage text-white font-bold rounded-full w-9 h-9 flex items-center justify-center shadow-inner text-lg">
+          class="hidden bg-sage text-white font-bold rounded-full w-9 h-9 md:flex items-center justify-center shadow-inner text-lg">
           1
         </div>
-        <h1 class="text-lg font-bold text-dark tracking-tight">
+        <h1 class="pl-3 md:pl-0 text-lg font-bold text-dark tracking-tight">
           <span class="text-mocha">satu</span>undangan<span class="text-sage">.id</span>
         </h1>
       </div>
@@ -24,7 +59,7 @@
       <AuthModal :show="show" :authMode="authMode" @update:authMode="authMode = $event" @close="show = false" />
 
       <!-- CTA -->
-      <div class="flex items-center gap-4">
+      <div class="hidden md:flex items-center gap-4">
         <template v-if="userName">
           <div class="relative">
             <button @click="toggleDropdown" class="flex items-center gap-2 text-sm text-dark hover:text-mocha">
@@ -49,6 +84,13 @@
           </div>
         </template>
 
+        <template v-else>
+          <button class="text-sm text-mocha hover:text-accent-gold transition" @click="show = true">Login</button>
+          <button class="px-4 py-2 md:py-3 text-sm text-white bg-mocha hover:bg-mocha/90 rounded-xl"
+            @click="emit('create-invitation')">
+            Buat Undangan
+          </button>
+        </template>
       </div>
 
     </div>
@@ -64,6 +106,7 @@ import { onClickOutside } from '@vueuse/core'
 
 const show = ref(false)
 const authMode = ref('login')
+const sidebarOpen = ref(false)
 
 const auth = useAuthStore()
 const userName = computed(() => auth.user?.name || null)
@@ -78,7 +121,7 @@ onClickOutside(dropdownRef, () => {
 const toggleDropdown = () => {
   dropdownOpen.value = !dropdownOpen.value
 }
-
+const emit = defineEmits(['create-invitation'])
 
 const handleLogout = async () => {
   const result = await Swal.fire({
